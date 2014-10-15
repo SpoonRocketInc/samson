c42940f1fe386368c3f99b01a32c6b8d539ade4e
Vincent Lin <vincent@spoonrocket.com>
2014-10-15 16:45:08 +0800

    use dotenv setup 3rd party email service in smtp configurations

--- config/initializers/smtp.rb
+++ config/initializers/smtp.rb
@@ -1,5 +1,16 @@
-ActionMailer::Base.smtp_settings = {
-  :authentication => 'plain',
-  :enable_starttls_auto => false,
-  :openssl_verify_mode => 'none'
-}
+if ENV["ACTION_MAILER_SMTP_ADDRESS"]
+  ActionMailer::Base.smtp_settings = {
+    address:        ENV["ACTION_MAILER_SMTP_ADDRESS"],
+    port:           ENV["ACTION_MAILER_SMTP_PORT"],
+    domain:         ENV["ACTION_MAILER_SMTP_DOMAIN"],
+    authentication: ENV["ACTION_MAILER_SMTP_AUTH"],
+    user_name:      ENV["ACTION_MAILER_SMTP_USERNAME"],
+    password:       ENV["ACTION_MAILER_SMTP_PASSWORD"]
+  }
+else
+  ActionMailer::Base.smtp_settings = {
+    :authentication => 'plain',
+    :enable_starttls_auto => false,
+    :openssl_verify_mode => 'none'
+  }
+end
